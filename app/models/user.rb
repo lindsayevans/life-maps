64a502e471c17bc816499f1413f385047d5abf3a
Lindsay Evans <linz@linz.id.au>
2008-09-28 16:36:04 +1000

    Adding edit/view convenience methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+    has_many :timelines
+
+    def can_edit? timeline
+	self == timeline.user
+    end
+
+    def can_view? timeline
+	timeline.public || can_edit? timeline
+    end
+
 end
