0cf85096d1a260dc6b81aee9031f78bf2b68f0eb
Lindsay Evans <linz@linz.id.au>
2008-10-12 11:42:03 +1100

    making find_close_places work with metres

--- app/models/place.rb
+++ app/models/place.rb
@@ -34,8 +34,32 @@ class Place < ActiveRecord::Base
     return days / divisor
   end
 
-  def find_close_places(distance=1, limit=5)
+  def find_close_places_by_line_length(distance=1, limit=5)
     Place.find :all, :conditions => ["id != ? AND GLength(GeomFromText(CONCAT('LineString(? ?,',  X(coordinates), ' ', Y(coordinates),')'))) < ?", self[:id], self.latitude, self.longitude, distance], :limit => limit
   end
+
+  # Find close places by metres
+  def find_close_places(distance=1000, limit=5)
+    lat = self.latitude
+    lon = self.longitude
+    sql = "
+SELECT *
+FROM places
+WHERE id != #{self[:id]}
+  AND 6370997.0 * (2 * ATAN2(SQRT(POW(SIN(((#{lat} - X(coordinates)) * 0.0174532925199433)/2),2) + COS(X(coordinates) * 0.0174532925199433) * COS(#{lat} * 0.0174532925199433) * POW(SIN(((#{lon} - Y(coordinates)) * 0.0174532925199433)/2),2)), SQRT(1-(POW(SIN(((#{lat} - X(coordinates)) * 0.0174532925199433)/2),2) + COS(X(coordinates) * 0.0174532925199433) * COS(#{lat} * 0.0174532925199433) * POW(SIN(((#{lon} - Y(coordinates)) * 0.0174532925199433)/2),2))))) <= #{distance}
+LIMIT #{limit}"
+    Place.find_by_sql sql
+  end
+
+  def distance place
+    lat = place.latitude
+    lon = place.longitude
+    sql = "SELECT name, AsText(coordinates),
+6370997.0 * (2 * ATAN2(SQRT(POW(SIN(((#{lat} - X(coordinates)) * 0.0174532925199433)/2),2) + COS(X(coordinates) * 0.0174532925199433) * COS(#{lat} * 0.0174532925199433) * POW(SIN(((#{lon} - Y(coordinates)) * 0.0174532925199433)/2),2)), SQRT(1-(POW(SIN(((#{lat} - X(coordinates)) * 0.0174532925199433)/2),2) + COS(X(coordinates) * 0.0174532925199433) * COS(#{lat} * 0.0174532925199433) * POW(SIN(((#{lon} - Y(coordinates)) * 0.0174532925199433)/2),2))))) AS distance_in_metres
+FROM places;"
+    t = ActiveRecord::Base.connection.execute sql
+    #t[0].distance_in_metres # TODO: do it
+  end
+
 
 end
