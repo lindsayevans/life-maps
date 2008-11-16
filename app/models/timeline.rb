fd37e23d9ab13a8413e8ad4a73f7078d42db99e6
Lindsay Evans <linz@linz.id.au>
2008-11-16 19:21:05 +1100

    Adding place order back in now that VD works

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,7 +1,7 @@
 class Timeline < ActiveRecord::Base
 
   belongs_to :user
-  has_many :places, :dependent => :destroy#, :order => 'places.from_start ASC, places.from_end ASC'
+  has_many :places, :dependent => :destroy, :order => 'places.from_start ASC, places.from_end ASC'
   has_many :place_types, :through => :places
 
   #validates_uniqueness_of :friendly_url # if not empty
